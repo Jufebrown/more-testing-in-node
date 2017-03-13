a9ac31066668b9dfe37497b257c78c24c486521e
Jufebrown <jufebrown@gmail.com>
2017-03-13 14:49:15 -0500

    tested to make sure add() takes 2 args

--- test/add-test.js
+++ test/add-test.js
@@ -1,10 +1,14 @@
 `use strict`
 
-const {assert: {isNumber}} = require(`chai`)
+const {assert: {isNumber, equal}} = require(`chai`)
 const add = require(`../lib/add`)
 
 describe(`add`, () => {
   it(`should return a number`, () => {
     isNumber(add())
+  })
+
+  it(`should take 2 arguments`, () => {
+    equal(add.length, 2)
   })
 })
