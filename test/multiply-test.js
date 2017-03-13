c654b37740ccbc93d9b80d33753a046f69b90b28
Jufebrown <jufebrown@gmail.com>
2017-03-13 15:19:54 -0500

    wrote test to see if multiply.js returns number

--- test/multiply-test.js
+++ test/multiply-test.js
@@ -1,1 +1,18 @@
 `use strict`
+
+const {assert: {isNumber, equal}} = require(`chai`)
+const multiply = require(`../lib/multiply`)
+
+describe(`multiply`, () => {
+  it(`should return a number`, () => {
+    isNumber(multiply())
+  })
+
+  // it(`should take 2 arguments`, () => {
+  //   equal(subtract.length, 2)
+  // })
+
+  // it(`should subtract the second argument from the first`, () => {
+  //   equal(subtract(2,1), 1)
+  // })
+})
