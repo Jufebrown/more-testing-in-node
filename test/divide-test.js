19b53bae37fd04236cb33847905c969a0aaae353
Jufebrown <jufebrown@gmail.com>
2017-03-13 15:27:04 -0500

    wrote test to see if divide.js returns a number

--- test/divide-test.js
+++ test/divide-test.js
@@ -1,1 +1,18 @@
 `use strict`
+
+const {assert: {isNumber, equal}} = require(`chai`)
+const divide = require(`../lib/divide`)
+
+describe(`divide`, () => {
+  it(`should return a number`, () => {
+    isNumber(divide())
+  })
+
+  // it(`should take 2 arguments`, () => {
+  //   equal(divide.length, 2)
+  // })
+
+  // it(`should divide the numbers`, () => {
+  //   equal(divide(2,1), 2)
+  // })
+})
