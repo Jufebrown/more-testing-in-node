bee12518bfcd076b5071effbedf627379033578e
Jufebrown <jufebrown@gmail.com>
2017-03-14 10:47:38 -0500

    wrote test to see if calculate.js returns a number then made it pass

--- test/calculate-test.js
+++ test/calculate-test.js
@@ -1,1 +1,14 @@
 `use strict`
+
+const {assert: {isNumber}} = require(`chai`)
+const calculate = require(`../lib/calculate`)
+
+describe(`calculate`, () => {
+  it(`should output a number`, () => {
+    isNumber(calculate())
+  })
+
+  // it(`should get the user input as an array`, () => {
+  //   isArray(calculate())
+  // })
+})
