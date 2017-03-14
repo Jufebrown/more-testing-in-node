727a15d5fc517fecdb35a85c28017a17aaaac275
Jufebrown <jufebrown@gmail.com>
2017-03-14 10:05:09 -0500

    wrote test to see if parse-args.js returns an object with keys of num1, operator, and num2 and values of the array that is passed to it

--- test/parse-args-test.js
+++ test/parse-args-test.js
@@ -1,17 +1,25 @@
 `use strict`
 
-const {assert: {isObject, equal, property}} = require(`chai`)
+const {assert: {isObject, equal, property, propertyVal}} = require(`chai`)
 const parseArgs = require(`../lib/parse-args`)
 
 describe(`parseArgs`, () => {
-  it(`should return a number`, () => {
-    isObject(parseArgs())
+  it(`should return an object`, () => {
+    let testArgs = [4, '*', 2]
+    isObject(parseArgs(testArgs))
   })
 
   it(`should return an object with keys "num1", "operator" and "num2"`, () => {
-    property(parseArgs(), 'num1')
-    property(parseArgs(), 'operator')
-    property(parseArgs(), 'num2')
+    let testArgs = [4, '*', 2]
+    property(parseArgs(testArgs), 'num1')
+    property(parseArgs(testArgs), 'operator')
+    property(parseArgs(testArgs), 'num2')
   })
 
+  it(`should take an array of length 3 as an argument and output the elements of the array to num1, operator, and num2 respectively`, () => {
+    let testArgs = [4, '*', 2]
+    propertyVal(parseArgs(testArgs), 'num1', 4)
+    // propertyVal(parseArgs(testArgs), 'operator', '*')
+    // propertyVal(parseArgs(testArgs), 'num2', 2)
+  })
 })
